5f76c312498d4ecc324fa172293dbc3bb5067952
Jerry Jäppinen <eiskis@gmail.com>
2022-06-26 16:07:01 +0200

    Nuxt config

---
+++ README.md
@@ -0,0 +1,11 @@
+# linna-vue
+
+### Nuxt module
+
+Install as a Nuxt module to auto-import components.
+
+```js
+export default {
+  modules: ['linna-vue']
+}
+```
---
+++ nuxt.js
@@ -0,0 +1,16 @@
+import { defineNuxtModule } from '@nuxt/kit'
+import { fileURLToPath } from 'node:url'
+
+export default defineNuxtModule({
+  hooks: {
+    'components:dirs' (dirs) {
+      // Add ./components dir to the list
+      dirs.push({
+        path: fileURLToPath(new URL('./components', import.meta.url)),
+        // prefix: 'linna'
+        prefix: ''
+      })
+    }
+  }
+})
+
