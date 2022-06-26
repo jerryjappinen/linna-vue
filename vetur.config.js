ee1581b588955e2f2acee558c628a9517756643b
Jerry Jäppinen <eiskis@gmail.com>
2022-06-26 15:59:39 +0200

    Root config

---
+++ vetur.config.js
@@ -0,0 +1,16 @@
+// https://vuejs.github.io/vetur/guide/setup.html#advanced
+module.exports = {
+  settings: {
+    // "vetur.useWorkspaceDependencies": true,
+    // "vetur.experimental.templateInterpolationService": true
+  },
+
+  projects: [
+    {
+      root: './docs'
+      // globalComponents: [
+      //   './components/**/*.vue'
+      // ]
+    }
+  ]
+}
