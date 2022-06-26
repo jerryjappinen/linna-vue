3b57a56d803f0a03dd83b06240c31d534e74137c
Jerry Jäppinen <eiskis@gmail.com>
2022-06-26 13:40:08 +0000

    Basic setup

---
+++ components/AppStoreLink.vue
@@ -0,0 +1,52 @@
+<script>
+import AppStoreBadge from './AppStoreBadge'
+import ExternalLink from './ExternalLink'
+
+export default {
+
+  components: {
+    AppStoreBadge,
+    ExternalLink
+  },
+
+  props: {
+
+    appId: {
+      type: [Number, String],
+      required: true
+    },
+
+    refer: {},
+    here: {},
+    light: {}
+
+  },
+
+  computed: {
+
+    href () {
+      return this.appId
+        ? `https://itunes.apple.com/us/app/ab-classic/${this.appId}?mt=8`
+        : null
+    }
+
+  }
+
+}
+</script>
+
+<template>
+  <ExternalLink
+    class="c-app-store-link"
+    :href="href"
+    :here="here"
+    :refer="refer"
+  >
+    <slot>
+      <AppStoreBadge
+        class="c-app-store-link-badge"
+        :light="light"
+      />
+    </slot>
+  </external-link>
+</template>
---
+++ docs/pages/AppStoreBadge.vue
@@ -0,0 +1,25 @@
+<script>
+import AppStoreBadge from '../../components/AppStoreBadge'
+
+export default {
+  components: {
+    AppStoreBadge
+  }
+}
+</script>
+
+<template>
+  <div>
+
+    <h1>AppStoreBadge</h1>
+
+    <p>
+        <AppStoreBadge />
+    </p>
+
+    <p>
+        <AppStoreBadge :light="true" />
+    </p>
+
+  </div>
+</template>
