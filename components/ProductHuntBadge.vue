cbcf16aebaa1c04f6c0da44bd55c01d728c24566
Jerry Jäppinen <eiskis@gmail.com>
2022-07-20 22:03:01 +0200

    Deps

--- components/ProductHuntBadge.vue
+++ components/ProductHuntBadge.vue
@@ -1,62 +1,33 @@
 <script>
-import ExternalLink from './ExternalLink'
-
-export default {
-
-  components: {
-    ExternalLink
+const props = defineProps({
+  postId: {
+    type: [Number, String],
+    required: true
   },
 
-  props: {
-
-    postId: {
-      type: [Number, String],
-      required: true
-    },
-
-    postSlug: {
-      type: String,
-      required: true
-    },
-
-    title: {
-      type: String,
-      required: false
-    },
-
-    theme: {
-      type: String,
-      default: 'dark',
-      validator (value) {
-        return ['light', 'dark'].indexOf(value) !== -1
-      }
-    }
-
+  title: {
+    type: String,
+    required: false
   },
 
-  computed: {
-
-    href () {
-      return 'https://www.producthunt.com/posts/' + this.postSlug + '?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-' + this.postSlug
-    },
-
-    src () {
-      return 'https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=' + this.postId + '&theme=' + this.theme
-    }
-
+  light: {
+    type: Boolean,
+    default: false
   }
+})
 
-}
+const href = computed(() => {
+  return 'https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=' + props.postId + '&theme=' + (props.light ? 'light' : 'dark')
+})
 </script>
 
 <template>
-  <ExternalLink :href="href">
-    <img
-      :src="src"
-      :alt="title"
-      style="width: 250px; height: 54px;"
-      width="250px"
-      height="54px"
-    >
-  </ExternalLink>
+  <img
+    :src="src"
+    :alt="title"
+    style="width: 250px; height: 54px;"
+    width="250px"
+    height="54px"
+    class="c-product-hunt-badge"
+  >
 </template>
---
+++ components/ProductHuntLink.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ExternalLink from './ExternalLink'
+
+const props = defineProps({
+  postSlug: {
+    type: String,
+    required: true
+  }
+})
+
+const href = computed(() => {
+  return 'https://www.producthunt.com/posts/' + props.postSlug + '?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-' + props.postSlug
+})
+</script>
+
+<template>
+  <ExternalLink
+    :href="href"
+    class="c-product-hunt-link"
+  >
+    <slot />
+  </ExternalLink>
+</template>
