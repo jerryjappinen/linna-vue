1add979733f57a9567fbd854609d94b208c71965
Jerry Jäppinen <eiskis@gmail.com>
2022-07-27 21:05:17 +0200

    Compression

---
+++ nuxt.config/compression.js
@@ -0,0 +1,15 @@
+// Compression
+// https://github.com/exreplay/nuxt-compression
+export default (options) => {
+  return {
+    buildModules: ['@averjs/nuxt-compression', {
+      // maxAge: ...,
+      // cacheControl: ...,
+
+      // https://github.com/vbenjs/vite-plugin-compression#options
+      viteCompression: {
+        ...(options || {})
+      }
+    }]
+  }
+}
